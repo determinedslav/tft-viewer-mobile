306c62ddad110cc66745f0b3e4931f5defaa5eb2
DeterminedSlav <slavimitev10@gmail.com>
2020-02-13 17:48:57 +0200

    apikey updated

--- constants/Api.js
+++ constants/Api.js
@@ -1,7 +1,7 @@
 export default {
     protocol: "https://",
     apiLink: ".api.riotgames.com/tft/",
-    key: "?api_key=RGAPI-2256785e-570a-4565-8ec4-d1a367db701f",
+    key: "?api_key=RGAPI-e4c25864-4f8a-456d-9f5a-dc973ba0a655",
     nameApi: "summoner/v1/summoners/by-name/",
     statsApi: "league/v1/entries/by-summoner/",
     full: 'https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/determinedslav?api_key=RGAPI-73705406-5b6c-447d-bad5-90011d79a3cd',
