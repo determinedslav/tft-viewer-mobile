23ec31dd46844afecd79fe12c68ffcb4c6a76cfa
DeterminedSlav <slavimitev10@gmail.com>
2020-02-13 18:36:33 +0200

    updated card component

--- components/Card.js
+++ components/Card.js
@@ -6,61 +6,32 @@ import Button from '../components/Button';
 function Card(props) {
 
     return (
-        <View style = {styles.container}>
-            <View style = {styles.buttonContainer}>
-                <Button onPress={props.onPress} style = {styles.buttonDelete} title="X"/>
-            </View>
-            <View style = {styles.textContainer}>
-                <Text style = {[styles.text, styles.textRegion]}>{props.region}</Text>
-                <Text style = {[styles.text, styles.textName]}>{props.name}</Text>
-                <Text style = {styles.text}>{props.rank} {props.division}</Text>
-                <Text style = {styles.text}>{props.wins} Wins {props.lp}LP</Text>
-            </View>
+        <View style = {styles.cardContainer}>
+            <Text style = {styles.text}>{props.region}</Text>
+            <Text style = {[styles.text, styles.textName]}>{props.name}</Text>
+            <Text style = {styles.text}>{props.rank} {props.division}</Text>
+            <Text style = {styles.text}>{props.wins} Wins {props.lp}LP</Text>
         </View>
     )
 }
 const styles = StyleSheet.create({
-    container: {
+    cardContainer: {
         backgroundColor: Colors.containerBackground,
         padding: 10,
+        borderColor: Colors.headerBackground,
+        borderWidth: 5,
         borderRadius: 5,
         marginBottom: 15,
-    },
-    textContainer: {
         alignItems: 'center',
-        marginBottom: 20,
-    },
-    buttonContainer: {
-        marginBottom: 0,
     },
     text: {
         fontSize: 24,
         fontWeight: 'bold',
         marginVertical: 3,
     },
-    textRegion: {
-        fontSize: 22,
-        marginBottom: 10,
-    },
     textName: {
         fontSize: 22,
         marginBottom: 30,
     },
-
-    buttonUpdate: {
-        backgroundColor: Colors.primary,
-        borderColor: Colors.primary,
-        flex: 1,
-        marginRight: 100,
-        
-    },
-    buttonDelete: {
-        backgroundColor: Colors.danger,
-        borderColor: Colors.danger,
-        alignSelf: "flex-end",
-        width: 25,
-        height: 25,
-        padding: 0,
-    } 
 });
 export default Card;
